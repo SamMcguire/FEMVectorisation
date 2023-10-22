fcca096831deb199c2f9234f1e6a368d0c362d72
Sam McGuire <smcg0013@student.monash.edu>
2023-10-22 23:56:14 +1100

    Added tests

--- test.py
+++ test.py
@@ -9,29 +9,11 @@ geo = SplineGeometry()
 
 
 kr = 20
-penis = ImageVec('Images/Dino.png')
-segments = penis.GetVectorisation(kWidth = 50, blurRadius = 70, numSegments = 20, display = True)
+vec = ImageVec('Images/Dino.png')
+segments = vec.GetVectorisation(kWidth = 30, blurRadius = 60, numSegments = 20, 
+	display = True, saveFig = "")
 
-penis.GenerateGeometry(geo)
-
-print(segments)
-
-# pnts =[(0,0),
-#        #(0,0,0.05), # define a local mesh refinement for one point
-#        (1,0),
-#        (1,0.5),
-#        (1,1),
-#        (0.5,1),
-#        (0,1)]
-
-# p1,p2,p3,p4,p5,p6 = [geo.AppendPoint(*pnt) for pnt in pnts]
-# curves = [[["line",p1,p2],"bottom"],
-#           [["line",p2,p3],"right"],
-#           [["spline3",p3,p4,p5],"curve"],
-#           [["line",p5,p6],"top"],
-#           [["line",p6,p1],"left"]]
-
-# [geo.Append(c,bc=bc) for c,bc in curves]
-# ngmesh = geo.GenerateMesh(maxh=0.2)
-# print("test")
-# Draw (Mesh(ngmesh))
+vec.GenerateGeometry(geo)
+geo.SetDomainMaxH(4, 0.01)
+ngmesh = geo.GenerateMesh(maxh=0.1)
+mesh = Mesh(ngmesh)
